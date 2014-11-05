aa859d5df4ae970d22fd0ffcd6d5fc6b71696fb4
ryelle <kelly.dwan@gmail.com>
2014-11-05 15:27:26 -0500

    Merge Constants check

---
+++ checks/constants.php
@@ -0,0 +1,37 @@
+<?php
+
+class Constants_Check extends ThemeCheck {
+
+	function check( $php_files, $css_files, $other_files ) {
+		$pass = true;
+
+		$constants = array(
+			'STYLESHEETPATH' => 'get_stylesheet_directory()',
+			'TEMPLATEPATH'   => 'get_template_directory()',
+			'PLUGINDIR'      => 'WP_PLUGIN_DIR',
+			'MUPLUGINDIR'    => 'WPMU_PLUGIN_DIR',
+		);
+
+		ThemeCheck::increment(); // Keep track of how many checks we do.
+
+		foreach ( $php_files as $file_path => $file_contents ) {
+			foreach ( $constants as $invalid => $replacement ) {
+				if ( strpos( $file_contents, $invalid ) ) {
+					$file_name = basename( $file_path );
+					$line = \ThemeCheck\get_line( $invalid, $file_path );
+					$this->error[] = array(
+						'level' => TC_WARNING,
+						'file'  => $file_name,
+						'line'  => $line,
+						'error' => sprintf( '<code>%1$s</code> was found, use <code>%2$s</code> instead.', $invalid, $replacement ),
+						'test'  => __CLASS__,
+					);
+				}
+			}
+		}
+
+		return $pass;
+	}
+}
+
+$themechecks['constants'] = new Constants_Check;
