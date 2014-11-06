81f1a808a219fc3440e91ea997caec4e354302f5
ryelle <kelly.dwan@gmail.com>
2014-11-05 21:00:26 -0500

    Add the recommended headers tests to the headers check

--- checks/style-headers.php
+++ checks/style-headers.php
@@ -7,23 +7,50 @@ class Style_Headers_Check extends ThemeCheck {
 		$css  = implode( ' ', $css_files );
 
 		$tests = array(
-			'[ \t\/*#]*Theme Name:' => '<strong>Theme name:</strong> is missing from your style.css header.',
-			'[ \t\/*#]*Description:' => '<strong>Description:</strong> is missing from your style.css header.',
-			'[ \t\/*#]*Author:' => '<strong>Author:</strong> is missing from your style.css header.',
-			'[ \t\/*#]*Version' => '<strong>Version:</strong> is missing from your style.css header.',
-			'[ \t\/*#]*License:' => '<strong>License:</strong> is missing from your style.css header.',
-			'[ \t\/*#]*License URI:' => '<strong>License URI:</strong> is missing from your style.css header.',
+			array(
+				'level' => TC_REQUIRED,
+				'pattern' => '[ \t\/*#]*Theme Name:',
+				'reason' => '<strong>Theme name:</strong> is missing from your style.css header.',
+			), array(
+				'level' => TC_REQUIRED,
+				'pattern' => '[ \t\/*#]*Description:',
+				'reason' => '<strong>Description:</strong> is missing from your style.css header.',
+			), array(
+				'level' => TC_REQUIRED,
+				'pattern' => '[ \t\/*#]*Author:',
+				'reason' => '<strong>Author:</strong> is missing from your style.css header.',
+			), array(
+				'level' => TC_REQUIRED,
+				'pattern' => '[ \t\/*#]*Version',
+				'reason' => '<strong>Version:</strong> is missing from your style.css header.',
+			), array(
+				'level' => TC_REQUIRED,
+				'pattern' => '[ \t\/*#]*License:',
+				'reason' => '<strong>License:</strong> is missing from your style.css header.',
+			), array(
+				'level' => TC_REQUIRED,
+				'pattern' => '[ \t\/*#]*License URI:',
+				'reason' => '<strong>License URI:</strong> is missing from your style.css header.',
+			), array(
+				'level' => TC_RECOMMENDED,
+				'pattern' => '[ \t\/*#]*Theme URI:',
+				'reason' => '<strong>Theme URI:</strong> is missing from your style.css header.',
+			), array(
+				'level' => TC_RECOMMENDED,
+				'pattern' => '[ \t\/*#]*Author URI:',
+				'reason' => '<strong>Author URI:</strong> is missing from your style.css header.',
+			),
 		);
 
 		ThemeCheck::increment(); // Keep track of how many checks we do.
 
-		foreach ( $tests as $test => $reason ) {
-			if ( ! preg_match( '/' . $test . '/i', $css, $matches ) ) {
+		foreach ( $tests as $test ) {
+			if ( ! preg_match( '/' . $test['pattern'] . '/i', $css, $matches ) ) {
 				$this->error[] = array(
-					'level' => TC_REQUIRED,
+					'level' => $test['level'],
 					'file'  => false,
 					'line'  => false,
-					'error' => $reason,
+					'error' => $test['reason'],
 					'test'  => __CLASS__,
 				);
 				$pass = false;
