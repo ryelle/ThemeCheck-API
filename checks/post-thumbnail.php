8341829e9ed90863e453957efd3ba598c9c85857
ryelle <kelly.dwan@gmail.com>
2014-11-05 20:36:35 -0500

    Merge Post Thumbnail check

---
+++ checks/post-thumbnail.php
@@ -0,0 +1,35 @@
+<?php
+
+class Post_Thumbnail_Check extends ThemeCheck {
+
+	function check( $php_files, $css_files, $other_files ) {
+		$pass = true;
+		$php = implode( ' ', $php_files );
+
+		ThemeCheck::increment(); // Keep track of how many checks we do.
+
+		if ( strpos( $php, 'the_post_thumbnail' ) === false ) {
+			$this->error[] = array(
+				'level' => TC_RECOMMENDED,
+				'file'  => false,
+				'line'  => false,
+				'error' => 'No reference to <code>the_post_thumbnail()</code> was found in the theme. It is recommended that the theme implement this functionality instead of using custom fields for thumbnails.',
+				'test'  => __CLASS__,
+			);
+		}
+
+		if ( strpos( $php, 'post-thumbnails' ) === false ) {
+			$this->error[] = array(
+				'level' => TC_RECOMMENDED,
+				'file'  => false,
+				'line'  => false,
+				'error' => "No reference to post-thumbnails was found in the theme. If the theme has a thumbnail like functionality, it should be implemented with <code>add_theme_support( 'post-thumbnails' )</code>in the functions.php file.",
+				'test'  => __CLASS__,
+			);
+		}
+
+		return $pass;
+	}
+}
+
+$themechecks['post-thumbnail'] = new Post_Thumbnail_Check;
