f9a11347c05e844e42bcab809caa9ff3f209f123
ryelle <kelly.dwan@gmail.com>
2014-11-05 20:18:26 -0500

    Add non printable characters check

---
+++ checks/nonprintable.php
@@ -0,0 +1,31 @@
+<?php
+
+class NonPrintable_Check  extends ThemeCheck {
+
+	function check( $php_files, $css_files, $other_files ) {
+		$pass = true;
+
+		ThemeCheck::increment(); // Keep track of how many checks we do.
+
+		foreach ( $php_files as $file_path => $file_contents ) {
+			// 09 = tab
+			// 0A = line feed
+			// 0D = new line
+			if ( preg_match('/[\x00-\x08\x0B-\x0C\x0E-\x1F\x80-\xFF]/', $file_contents, $matches ) ) {
+				$file_name = basename( $file_path );
+				$line = \ThemeCheck\get_line( $matches[0], $file_path );
+				$this->error[] = array(
+					'level' => TC_INFO,
+					'file'  => $file_name,
+					'line'  => $line,
+					'error' => sprintf( 'Non-printable characters <code>%1$s</code> were found in the <strong>%2$s</strong> file. You may want to check this file for errors.', htmlentities( $matches[0] ), $file_name ),
+					'test'  => __CLASS__,
+				);
+			}
+		}
+
+		return $pass;
+	}
+}
+
+$themechecks['nonprintable'] = new NonPrintable_Check;
