5833739967fc898f9af55d7f43fe1d90947987ab
ryelle <kelly.dwan@gmail.com>
2014-11-11 11:59:34 -0500

    Move the post_thumbnail functions check so that it’s only run if we already see `post-thumbnails`. Otherwise we’re warning twice if a theme doesn’t support post thumbnails.

--- checks/post-thumbnail.php
+++ checks/post-thumbnail.php
@@ -8,22 +8,20 @@ class Post_Thumbnail_Check extends ThemeCheck {
 
 		ThemeCheck::increment(); // Keep track of how many checks we do.
 
-		if ( ( strpos( $php, 'the_post_thumbnail' ) === false ) && ( strpos( $php, 'get_post_thumbnail' ) === false ) ) {
+		if ( strpos( $php, 'post-thumbnails' ) === false ) {
+			$this->error[] = array(
+				'level' => TC_INFO,
+				'file'  => false,
+				'line'  => false,
+				'error' => "No reference to post-thumbnails was found in the theme. If the theme has a thumbnail like functionality, it should be implemented with <code>add_theme_support( 'post-thumbnails' )</code>in the functions.php file.",
+				'test'  => __CLASS__,
+			);
+		} elseif ( ( strpos( $php, 'the_post_thumbnail' ) === false ) && ( strpos( $php, 'get_post_thumbnail' ) === false ) ) {
 			$this->error[] = array(
 				'level' => TC_RECOMMENDED,
 				'file'  => false,
 				'line'  => false,
 				'error' => 'No reference to <code>the_post_thumbnail()</code> was found in the theme. It is recommended that the theme implement this functionality instead of using custom fields for thumbnails.',
-				'test'  => __CLASS__,
-			);
-		}
-
-		if ( strpos( $php, 'post-thumbnails' ) === false ) {
-			$this->error[] = array(
-				'level' => TC_RECOMMENDED,
-				'file'  => false,
-				'line'  => false,
-				'error' => "No reference to post-thumbnails was found in the theme. If the theme has a thumbnail like functionality, it should be implemented with <code>add_theme_support( 'post-thumbnails' )</code>in the functions.php file.",
 				'test'  => __CLASS__,
 			);
 		}
